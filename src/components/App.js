df9971bdc5b85b145610492d2ae82f45c36b82f8
podmar <marta.podziewska@gmail.com>
2022-05-18 16:26:01 +0200

    implemented the details and home view (test) and added the booklist to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../views/Home.js';
+import Detail from '../views/Detail.js';
+import BookList from './BookList';
 
 function App() {
   return (
-    <div className='App'>
-      <p>This is a test paragraph.</p>
-      <h1>And a test heading here.</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="detail" element={<Detail />} />
+      </Routes>
+      
+      <div className='App'>
+        <p>This is a test paragraph.</p>
+        <h1>And a test heading here.</h1>
+      </div>     
+      <BookList />
+    </>
   );
 }
 

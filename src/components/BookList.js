3b46fb8a6c1ce50ae177de39ad8bc0a58d2c2e1f
podmar <marta.podziewska@gmail.com>
2022-05-18 16:24:56 +0200

    fetched a list of titles from the books API and displayed them

---
+++ src/components/BookList.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import data from '../config.json'
+
+function BookList() {
+    const url = "https://www.googleapis.com/books/v1/volumes?q=climbing&key="+data.API_KEY;
+
+    const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        try {
+            console.log(url);
+            const response = await fetch(url);
+            const bookData = await response.json();
+            const bookList = await bookData.items;
+            setBooks(bookList);
+            console.log(books, bookList);
+        } catch (err) {
+            console.log(err);
+        };
+    };
+    useEffect(() => {
+        fetchBooks()
+    }, [])
+
+  return (
+    <>
+    <h5>Here are the books:</h5>
+    <div>
+        {books.map(book => { 
+            return (
+                <div>{book.volumeInfo.title}</div>
+            )
+        }
+        )}
+    </div>
+    </>
+  )
+}
+
+export default BookList

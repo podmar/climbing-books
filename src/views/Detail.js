f37f6f4ce4581e3a239d8675ec38bca8d2e9a4cb
podmar <marta.podziewska@gmail.com>
2022-05-18 16:23:48 +0200

    created views and added a test detail view

---
+++ src/views/Detail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import BookList from '../components/BookList'
+
+function Detail() {
+  return (
+    <div>
+    <h1>Detail</h1>
+    <p>Welcome to the detail view.</p>
+    {/* <BookList/> */}
+    </div>
+  )
+}
+
+export default Detail

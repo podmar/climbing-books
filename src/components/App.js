b98855bb4126becda6a88fc676d1f372366548dc
podmar <marta.podziewska@gmail.com>
2022-05-19 11:50:54 +0200

    added errorPage view

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Detail from '../views/Detail.js';
 import LogIn from '../views/LogIn.js';
 import ChatRoom from '../views/ChatRoom.js';
 import Data from '../views/Data.js';
+import ErrorPage from '../views/ErrorPage.js';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="login" element={<LogIn />} />
         <Route path="chatroom" element={<ChatRoom />} />
         <Route path="data" element={<Data />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
   )

38f9ca5a0a2db5c1baf365d81f4ae4eab51617e3
podmar <marta.podziewska@gmail.com>
2022-05-19 11:48:24 +0200

    added Data component for info about the API

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,20 @@ import Home from '../views/Home.js';
 import Detail from '../views/Detail.js';
 import LogIn from '../views/LogIn.js';
 import ChatRoom from '../views/ChatRoom.js';
+import Data from '../views/Data.js';
 
 function App() {
   return (
     <>
-
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="detail" element={<Detail />} />
         <Route path="login" element={<LogIn />} />
         <Route path="chatroom" element={<ChatRoom />} />
+        <Route path="data" element={<Data />} />
       </Routes>
     </>
-  );
+  )
 }
 
 export default App;

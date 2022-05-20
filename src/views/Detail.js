d84f34aec08af2ada8c627c6a6e314bce9fd4eea
podmar <marta.podziewska@gmail.com>
2022-05-20 12:37:46 +0200

    Bugfixing the fetch function - NOT FUNCTIONAL

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -3,17 +3,20 @@ import { useParams } from 'react-router-dom';
 import data from '../config.json';
 
 function Detail() {
+  const [book, setBook] = useState([]);
   let { bookid } = useParams();
+  console.log(bookid)
 
   const url = `https://www.googleapis.com/books/v1/volumes?q=${bookid}&key=${data.API_KEY}`
-
-  const [book, setBook] = useState();
+console.log(url)
 
   const fetchBook = async () => {
     try {
       const response = await fetch(url);
       const bookData = await response.json();
-      const bookDetail = await bookData.items[0];
+      console.log(response, bookData)
+      const bookDetail =  bookData.items[0];
+      console.log(bookDetail)
       setBook(bookDetail);
     } catch (err) {
       console.log(err);
@@ -21,8 +24,11 @@ function Detail() {
 
   }
   useEffect(() => {
+    console.log("first")
     fetchBook()
+    console.log(book)
   },[])
+  
 
   return (
     <>
@@ -30,11 +36,15 @@ function Detail() {
     <div>    
       <h1>Detail</h1>
       <p>Welcome to the detail view.</p>
-      <p>{`This is the detailed view of the book ${ book.volumeInfo.title }.`}</p>
+      {/* {console.log(book.volumeInfo.title)} */}
+      {book &&  <p>{ book.volumeInfo.title }</p> }
+      {/* <p>{`This is the detailed view of the book ${ book.volumeInfo.title }.`}</p> */}
+      {/* <p>{ book.volumeInfo.title }</p> */}
+      {/* <p>{`this is the book ${ book.volumeInfo.title }`}</p> */}
     </div>
 
     <div>
-
+{/* placeholder for a separate component with a book view. IS IT NEEDED? WILL IT BE REUSED? */}
     </div>
     </>
   )

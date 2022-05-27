81c67013d3f1b2ea6963e583b00cefc73dfed3e7
podmar <marta.podziewska@gmail.com>
2022-05-27 20:05:01 +0200

    fixed a typo in user

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
-    const [uset, setUser] = useState(null)
+    const [user, setUser] = useState(null)
     
     return (
         <AuthContext.Provider

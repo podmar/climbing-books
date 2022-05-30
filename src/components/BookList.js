7af471b4c83ee875bc53e795913ff1d1569030fd
podmar <marta.podziewska@gmail.com>
2022-05-30 17:18:50 +0200

    implemented pagination

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import SeeDetailButton from './SeeDetailButton';
+import ReactPaginate from 'react-paginate';
 
 function BookList() {
-    // let startIndex = 0;
-
     const [books, setBooks] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
     const [totalBookCount, setTotalBookCount] = useState(0)
     const [totalPageCount, setTotalPageCount] = useState(0)
-    const [url, setUrl] = useState("https://www.googleapis.com/books/v1/volumes?q=climbing&key="+process.env.REACT_APP_GOOGLE_BOOKS_API_KEY)
-
-    // const url = "https://www.googleapis.com/books/v1/volumes?q=climbing&key="+process.env.REACT_APP_GOOGLE_BOOKS_API_KEY+"&startIndex="+currentIndex;
+    const [url, setUrl] = useState("https://www.googleapis.com/books/v1/volumes?q=climbing&key="+process.env.REACT_APP_GOOGLE_BOOKS_API_KEY+"&startIndex=");
 
     const fetchBooks = async () => {
         try {
-            const response = await fetch(url);
+            const response = await fetch(url+currentIndex);
             const bookData = await response.json();
             const bookCount = await bookData.totalItems;
             setTotalBookCount(bookCount);
+            const numberOfPages = Math.ceil(bookCount/10)
+            setTotalPageCount(numberOfPages);
             const bookList = await bookData.items;
             setBooks(bookList);
         } catch (err) {
@@ -25,12 +24,12 @@ function BookList() {
         };
     };
 
-    const countPages = () => {
-        let numberOfPages = totalBookCount.Math.ceil(10)
-        setTotalPageCount(numberOfPages);
+    const handlePageClick = (event) => {
+        // console.log(event);
+        // console.log(event.selected);
+        setCurrentIndex(event.selected)
+        console.log(currentIndex);
     };
-
-
 
     useEffect(() => {
         fetchBooks()
@@ -39,7 +38,7 @@ function BookList() {
   return (
     <>
     <h5>Here are the books:</h5>
-    {totalBookCount ? <p>A total of {totalBookCount} results.</p> : "Loading..." }
+    {totalBookCount ? <p>A total of {totalBookCount} results, page {currentIndex+1}</p> : "Loading..." }
     <div>
         {books && books.map(book => { 
             return (
@@ -53,6 +52,16 @@ function BookList() {
         }
         )}
     </div>
+    {totalPageCount && 
+        <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={2}
+        pageCount={totalPageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        />}
     </>
   )
 }

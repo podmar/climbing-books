cda98891331253c3166807b5fb5bc496e143daae
podmar <marta.podziewska@gmail.com>
2022-05-30 15:50:42 +0200

    created state variables for pagination: book count, page count, current index, url and a function to count pages; updated useEffect to render on change of currentIndex

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,29 +5,43 @@ function BookList() {
     // let startIndex = 0;
 
     const [books, setBooks] = useState([]);
-    const [page, setPage] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [totalBookCount, setTotalBookCount] = useState(0)
+    const [totalPageCount, setTotalPageCount] = useState(0)
+    const [url, setUrl] = useState("https://www.googleapis.com/books/v1/volumes?q=climbing&key="+process.env.REACT_APP_GOOGLE_BOOKS_API_KEY)
 
-    const url = "https://www.googleapis.com/books/v1/volumes?q=climbing&key="+process.env.REACT_APP_GOOGLE_BOOKS_API_KEY+"&startIndex="+page;
+    // const url = "https://www.googleapis.com/books/v1/volumes?q=climbing&key="+process.env.REACT_APP_GOOGLE_BOOKS_API_KEY+"&startIndex="+currentIndex;
 
     const fetchBooks = async () => {
         try {
             const response = await fetch(url);
             const bookData = await response.json();
+            const bookCount = await bookData.totalItems;
+            setTotalBookCount(bookCount);
             const bookList = await bookData.items;
             setBooks(bookList);
         } catch (err) {
             console.log(err);
         };
     };
+
+    const countPages = () => {
+        let numberOfPages = totalBookCount.Math.ceil(10)
+        setTotalPageCount(numberOfPages);
+    };
+
+
+
     useEffect(() => {
         fetchBooks()
-    }, [])
+    }, [currentIndex])
 
   return (
     <>
     <h5>Here are the books:</h5>
+    {totalBookCount ? <p>A total of {totalBookCount} results.</p> : "Loading..." }
     <div>
-        {books.map(book => { 
+        {books && books.map(book => { 
             return (
                 <div
                 key={book.id}
@@ -35,7 +49,6 @@ function BookList() {
                     {book.volumeInfo.title} 
                     <SeeDetailButton id={book.id} />
                 </div>
-
             )
         }
         )}

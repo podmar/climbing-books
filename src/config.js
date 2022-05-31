30a38ffb6e1b86a3a0490361aac1181f6436d9fe
podmar <marta.podziewska@gmail.com>
2022-05-31 16:28:29 +0200

    imported getAuth service

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,3 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);

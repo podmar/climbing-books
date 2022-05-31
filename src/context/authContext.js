a3fe6a40deedb2ba7393065e7faebc637496a4c4
podmar <marta.podziewska@gmail.com>
2022-05-31 16:55:07 +0200

    logic for the register function enabling registration/sign in in firebase

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,13 +1,34 @@
 import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config.js"
+
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
     const [user, setUser] = useState(null)
     
+    const register = (email, password) => {
+        console.log("email + password", email, password)
+
+        //transform into asynch await
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(userCredential)
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+    };
+
     return (
         <AuthContext.Provider
-        value={ {user, setUser} }
+        value={ {user, setUser, register} }
         >
             {props.children}
         </AuthContext.Provider>

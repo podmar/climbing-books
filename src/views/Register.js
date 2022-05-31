181d19d45842bf2e65bde32c071d70813fd3317a
podmar <marta.podziewska@gmail.com>
2022-05-31 15:47:31 +0200

    created input fields & labels for registration plus handler functions for the values and imported register function from auth context

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { Button } from 'react-bootstrap';
 import GoToButton from '../components/GoToButton'
+import { AuthContext } from '../context/AuthContext';
 
 function Register() {
+  const {register} = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleRegister = (event) => {
+    console.log("registered");
+    register(email, password)
+  };
+
   return (
     <>
       <div>Register</div>
-      <GoToButton path="/detail" element="detail page" />
+      <div>
+        <label htmlFor="email">email</label>
+        <input type="email" name="email" id="email" onChange={handleEmailChange} 
+        // value={email}
+        />
+        <label htmlFor="password"></label>
+        <input type="password" name="password" id="password" onChange={handlePasswordChange} 
+        // value={password}
+        />
+        <Button type="primary" onClick={handleRegister}>Register</Button>
+      </div>
+      <GoToButton path="/" element="home page" />
     </>
   )
 }

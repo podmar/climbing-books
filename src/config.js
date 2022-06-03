e6f6423b5734ab829c2753b48615a8cbc1001bae
podmar <marta.podziewska@gmail.com>
2022-06-03 12:41:06 +0200

    initialised firestore

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +16,5 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+// Initalise Firestore
+export const db = getFirestore(app);

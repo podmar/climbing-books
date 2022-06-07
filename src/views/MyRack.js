4c69616196b3eb8c21c104b60c4a56278aafaca5
podmar <marta.podziewska@gmail.com>
2022-06-07 12:16:45 +0200

    added auth context and displayed a user greeting

--- src/views/MyRack.js
+++ src/views/MyRack.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Notes from '../components/Notes'
+import { AuthContext } from '../context/AuthContext'
 
 function MyRack() {
+  const { user } = useContext(AuthContext)
+  
   return (
     <>
     <Row className='justify-content-center py-5 mt-4'>
       <Col>
         <h1>my rack</h1>
+        {user && 
+        <h5>hi {user.email.slice(0, user.email.indexOf("@"))}!</h5>
+        }
         <h5>here are the books placed on your rack</h5>
       </Col>
     </Row>

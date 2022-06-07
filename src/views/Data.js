ebbdb4dc60e8b1e011d8b07db9b5c79f5fb77fd3
podmar <marta.podziewska@gmail.com>
2022-06-07 17:49:49 +0200

    adjusted layout of the button

--- src/views/Data.js
+++ src/views/Data.js
@@ -5,12 +5,16 @@ import GoToButton from '../components/GoToButton'
 function Data() {
   return (
     <>
-    <Row className='justify-content-center py-5  mt-4'>
+      <Row className='justify-content-center py-5  mt-4'>
         <Col>
           <h1>data</h1>
           <h5>there is no content here just yet</h5>
-          <GoToButton path="/" element="home page"  />
         </Col>
+        <Row>
+          <Col>
+            <GoToButton path="/" element="home page"  />
+          </Col>
+        </Row>
       </Row>
     </>
   )

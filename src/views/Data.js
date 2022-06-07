0a3c3d288d1ac1c8ef829d82c168d8fc18f00ad1
podmar <marta.podziewska@gmail.com>
2022-06-07 17:36:37 +0200

    changed text on the go home button

--- src/views/Data.js
+++ src/views/Data.js
@@ -9,7 +9,7 @@ function Data() {
         <Col>
           <h1>data</h1>
           <h5>there is no content here just yet</h5>
-          <GoToButton path="/" element="Home"  />
+          <GoToButton path="/" element="home page"  />
         </Col>
       </Row>
     </>

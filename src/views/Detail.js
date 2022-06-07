670efcd68890ed40c843fcc02d2d678888495167
podmar <marta.podziewska@gmail.com>
2022-06-07 17:56:32 +0200

    removed redundant comments

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -48,16 +48,6 @@ function Detail() {
             <h5>Add a note and place the book on your rack</h5>
               <Form.Control type="text" placeholder="What would you like to remember about this book?" value={note} onChange={handleNoteChange}/>
             </Form.Group>
-
-{/* 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group> */}
-
             <Button variant="primary" type="submit" onClick={handleNoteSave}>
               Save your note
             </Button>

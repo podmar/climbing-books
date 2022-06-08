4288381cdf73d50eeeb6cd39c61f0c2d0d031a3b
podmar <marta.podziewska@gmail.com>
2022-06-08 12:01:39 +0200

    moved the code to save a note into a separate component

--- src/components/SaveNote.js
+++ src/components/SaveNote.js
@@ -1,16 +1,48 @@
 import React from 'react'
+import { useContext, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import { collection, addDoc } from "firebase/firestore"; 
+import { db } from '../config';
 
 function SaveNote() {
+  const [note, setNote] = useState("");
+  const {user} = useContext(AuthContext);
+  const { bookid } = useParams();
+  const navigateTo = useNavigate();
+
+  const handleNoteChange = (event) => {
+    setNote(event.target.value);
+  }; 
+
+  const handleNoteSave = async (event) => {
+    event.preventDefault()
+    const noteObj = {
+      text: note,
+      user: user.email,
+      date: new Date(),
+      book_id: bookid,
+    }
+    try {
+      const docRef = await addDoc(collection(db, "notes"), noteObj);
+      console.log("Document written with ID: ", docRef.id);
+      navigateTo("/myrack");
+      event.target.value=""
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <>
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Add a note</Form.Label>
-              <Form.Control type="text" placeholder="What would you like to remember about this book?" />
+          <Form>
+            <Form.Group className="my-3">
+            <h5>Add a note and place the book on your rack</h5>
+              <Form.Control type="text" placeholder="What would you like to remember about this book?" value={note} onChange={handleNoteChange}/>
             </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Save the note
+            <Button variant="primary" type="submit" onClick={handleNoteSave}>
+              Save your note
             </Button>
           </Form>
     </>
--- src/views/Detail.js
+++ src/views/Detail.js
@@ -1,59 +1,16 @@
-import { useContext, useState } from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Col, Row } from 'react-bootstrap';
 import BookDetail from '../components/BookDetail';
-// import GoToButton from '../components/GoToButton';
-import { AuthContext } from '../context/AuthContext';
-import { collection, addDoc } from "firebase/firestore"; 
-import { db } from '../config';
 import GoBackButton from '../components/GoBackButton';
+import SaveNote from '../components/SaveNote';
 
 function Detail() {
-  const [note, setNote] = useState("");
-  const {user} = useContext(AuthContext);
-  const { bookid } = useParams();
-  const navigateTo = useNavigate();
-
-
-  const handleNoteChange = (event) => {
-    setNote(event.target.value);
-    console.log(note);
-  }; 
-
-  const handleNoteSave = async (event) => {
-    event.preventDefault()
-    const noteObj = {
-      text: note,
-      user: user.email,
-      date: new Date(),
-      book_id: bookid,
-    }
-    try {
-      const docRef = await addDoc(collection(db, "notes"), noteObj);
-      console.log("Document written with ID: ", docRef.id);
-      navigateTo("/myrack");
-      event.target.value=""
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <>
       <Row className='justify-content-center py-5 mt-4'>
         <Col>
           <BookDetail />
-          {/* <GoToButton path="/" element="home page" /> */}
           <GoBackButton/>
-          <Form>
-            <Form.Group className="my-3">
-            <h5>Add a note and place the book on your rack</h5>
-              <Form.Control type="text" placeholder="What would you like to remember about this book?" value={note} onChange={handleNoteChange}/>
-            </Form.Group>
-            <Button variant="primary" type="submit" onClick={handleNoteSave}>
-              Save your note
-            </Button>
-          </Form>
+          <SaveNote/> 
         </Col>
       </Row>
     </>

b7cb633aee4ff7b7c61582026c863cbb96315696
podmar <marta.podziewska@gmail.com>
2022-08-04 10:56:21 +0200

    removed a redundant console log

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,78 +1,81 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { auth } from "../config.js"
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../config.js";
 import { useNavigate } from "react-router-dom";
-
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
-    const [user, setUser] = useState(null)
-    const redirectTo = useNavigate();
+  const [user, setUser] = useState(null);
+  const redirectTo = useNavigate();
 
-    const register = async (email, password) => {
-        console.log("email + password", email, password)
+  const register = async (email, password) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      // setUser(userCredential.user.email);
+      setUser(userCredential.user);
+      console.log(userCredential);
+      redirectTo("myrack");
+    } catch (error) {
+      console.log(error);
+      setUser(null);
+    }
+  };
 
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-            // setUser(userCredential.user.email);
-            setUser(userCredential.user);
-            console.log(userCredential)
-            redirectTo("myrack")
-        } catch (error) {
-            console.log(error)
-            setUser(null)
-            }
-        }
+  const login = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        console.log(userCredential.user);
+        // setUser(userCredential.user.email)
+        setUser(userCredential.user);
+        redirectTo("myrack");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setUser(null);
+        console.log(errorCode, errorMessage);
+      });
+  };
 
-    const login = (email, password) => {
+  const checkIfUserLoggedin = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  };
 
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                console.log(userCredential.user)
-                // setUser(userCredential.user.email)
-                setUser(userCredential.user)
-                redirectTo("myrack")
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                setUser(null)
-                console.log(errorCode, errorMessage);
-            });
-    };
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const checkIfUserLoggedin = () => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                const uid = user.uid;
-                setUser(user);
-            } else {
-                setUser(null);
-            }
-        });
-    };
+  useEffect(() => {
+    checkIfUserLoggedin();
+  }, []);
 
-    const logout = () => {
-        signOut(auth)
-        .then(() => {
-            setUser(null)
-        }).catch((error) => {
-            console.log(error);
-        })
-    };
-
-    useEffect(() => {
-      checkIfUserLoggedin();
-    }, [])
-    
-
-    return (
-        <AuthContext.Provider
-        value={ {user, setUser, register, login, logout} }
-        >
-            {props.children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, setUser, register, login, logout }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
